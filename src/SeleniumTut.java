993a29b2bdf9b67f127686dd9844e9c19532700b
Sibaaslam <sibaaslam@gmail.com>
2024-06-05 23:53:27 +0530

    Branch of user is developed

--- src/SeleniumTut.java
+++ src/SeleniumTut.java
@@ -4,6 +4,7 @@ public class SeleniumTut {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Selenium");
+		System.out.println("Selenium1");
 
 	}
 
